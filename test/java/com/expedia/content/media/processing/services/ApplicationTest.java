08033e2422f3936c0150aa3ce7b439df1c848ca1
valarcon <valarcon@expedia.com>
2015-07-29 14:30:03 -0400

    Add Application tests for JSON strings

---
+++ test/java/com/expedia/content/media/processing/services/ApplicationTest.java
@@ -0,0 +1,68 @@
+package com.expedia.content.media.processing.services;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Application.class)
+public class ApplicationTest {
+
+    @Autowired
+    private Application application;
+
+    @Test
+    public void testLoadContext() {
+        assertNotNull(application);
+    }
+
+    @Test
+    public void testAlphanumericStringExpediaIdInJsonMessage() throws Exception {
+        String jsonMessage = "{  \n" +
+                "   \"mediaProviderId\":\"1001\",\n" +
+                "   \"fileUrl\":\"http://images.com/dir1/img1.jpg\",\n" +
+                "   \"imageType\":\"Lodging\",\n" +
+                "   \"stagingKeyMap\":{  \n" +
+                "      \"externalId\":\"222\",\n" +
+                "      \"providerId\":\"300\",\n" +
+                "      \"sourceId\":\"99\"\n" +
+                "   },\n" +
+                "   \"expediaId\":\"NOT_A_NUMBER\",\n" +
+                "   \"categoryId\":\"801\",\n" +
+                "   \"callBack\":\"http://multi.source.callback/callback\",\n" +
+                "   \"caption\":\"caption\"\n" +
+                "}";
+        ResponseEntity<?> responseEntity = application.acquireMedia(jsonMessage);
+        assertNotNull(responseEntity);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void testAlphanumericConstantExpediaIdInJsonMessage() throws Exception {
+        String jsonMessage = "{  \n" +
+                "   \"mediaProviderId\":\"1001\",\n" +
+                "   \"fileUrl\":\"http://images.com/dir1/img1.jpg\",\n" +
+                "   \"imageType\":\"Lodging\",\n" +
+                "   \"stagingKeyMap\":{  \n" +
+                "      \"externalId\":\"222\",\n" +
+                "      \"providerId\":\"300\",\n" +
+                "      \"sourceId\":\"99\"\n" +
+                "   },\n" +
+                "   \"expediaId\":NOT_A_NUMBER,\n" +
+                "   \"categoryId\":\"801\",\n" +
+                "   \"callBack\":\"http://multi.source.callback/callback\",\n" +
+                "   \"caption\":\"caption\"\n" +
+                "}";
+        ResponseEntity<?> responseEntity = application.acquireMedia(jsonMessage);
+        assertNotNull(responseEntity);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+
+}
