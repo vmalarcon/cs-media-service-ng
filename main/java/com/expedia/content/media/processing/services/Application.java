3f853d184766d2640820814fab8707496439c3db
senli <seli@expedia.com>
2016-01-20 13:31:06 -0500

    add caption

--- main/java/com/expedia/content/media/processing/services/Application.java
+++ main/java/com/expedia/content/media/processing/services/Application.java
@@ -181,7 +181,7 @@ public class Application extends SpringBootServletInitializer {
             ImageMessage.ImageMessageBuilder imageMessageBuilder = new ImageMessage.ImageMessageBuilder();
 
             imageMessageBuilder = imageMessageBuilder.transferAll(imageMessage);
-            ImageMessage imageMessageNew = imageMessageBuilder.clientId(userName).mediaGuid(guid).requestId(String.valueOf(headers.get(REQUESTID))).build();
+            ImageMessage imageMessageNew = imageMessageBuilder.clientId(userName).mediaGuid(guid).caption("").requestId(String.valueOf(headers.get(REQUESTID))).build();
             /* TODO Change fileName in image message to be built with a hash of the url except when the client is multisource. 
              * Multisource will have to use the eid and provider id still until the filename keys are replaced in the fingerprint table.
              */
