6d32607c1de5c2ada33d96279f1071273df962ae
senli <seli@expedia.com>
2016-01-22 15:08:26 -0500

    remove useless queue

--- main/java/com/expedia/content/media/processing/services/validator/S3Validator.java
+++ main/java/com/expedia/content/media/processing/services/validator/S3Validator.java
@@ -2,16 +2,18 @@ package com.expedia.content.media.processing.services.validator;
 
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
-
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class S3Validator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(S3Validator.class);
 
     private static final String S3_CMD = "aws s3 ls ";
 
     public static boolean checkFileExists(String fileUrs) {
         if (fileUrs != null && fileUrs.contains("s3")) {
             String s = executeCommand(S3_CMD + fileUrs);
+            LOGGER.info("checkFileExists s3 output:"+s);
             if (s != null && s.contains(FilenameUtils.getBaseName(fileUrs))) {
                 return true;
             }
