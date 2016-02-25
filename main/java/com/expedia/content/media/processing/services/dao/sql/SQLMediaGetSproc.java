00f2638ff73cb16f1ae68e8a96c1a5f18f00d842
Andre Caron <acaron@YULC02P60BZG3QN.sea.corp.expecn.com>
2016-02-25 12:20:13 -0500

    CSPB-531782 Fixed media provider from content provider

--- main/java/com/expedia/content/media/processing/services/dao/sql/SQLMediaGetSproc.java
+++ main/java/com/expedia/content/media/processing/services/dao/sql/SQLMediaGetSproc.java
@@ -46,7 +46,7 @@ public class SQLMediaGetSproc extends StoredProcedure {
             return LcmMedia.builder()
                     .domainId(resultSet.getInt("CatalogItemID"))
                     .mediaId(resultSet.getInt("MediaID"))
-                    .provider(resultSet.getInt("ContentProviderId"))
+                    .provider(resultSet.getInt("MediaProviderID"))
                     .active(activeFlag != null && "A".equals(activeFlag) ? true : false)
                     .fileName(resultSet.getString("ContentProviderMediaName"))
                     .width(resultSet.getInt("MediaWidth"))
