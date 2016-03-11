9c5395fde0affcc3ddf95f7eab0a48addc0a6e0b
sstannus <t-sstannus@expedia.com>
2016-03-11 09:46:25 -0500

    CSPB-531586 - Lombok'd TempDerivativeMessage

--- main/java/com/expedia/content/media/processing/services/reqres/TempDerivativeMessage.java
+++ main/java/com/expedia/content/media/processing/services/reqres/TempDerivativeMessage.java
@@ -1,50 +1,16 @@
 package com.expedia.content.media.processing.services.reqres;
 
+
+import lombok.Data;
+
+
 /**
  * Represents a Temporary Derivative Message sent to media/v1/tempderivative
  */
-public class TempDerivativeMessage {
-    private String fileUrl;
-    private String rotation;
-    private Integer width;
-    private Integer height;
-
-    public TempDerivativeMessage(String fileUrl, String rotation, Integer width, Integer height) {
-        this.fileUrl = fileUrl;
-        this.rotation = rotation;
-        this.width = width;
-        this.height = height;
-    }
-
-    public String getFileUrl() {
-        return fileUrl;
-    }
-
-    public void setFileUrl(String fileUrl) {
-        this.fileUrl = fileUrl;
-    }
-
-    public String getRotation() {
-        return rotation;
-    }
-
-    public void setRotation(String rotation) {
-        this.rotation = rotation;
-    }
-
-    public Integer getWidth() {
-        return width;
-    }
-
-    public void setWidth(Integer width) {
-        this.width = width;
-    }
-
-    public Integer getHeight() {
-        return height;
-    }
-
-    public void setHeight(Integer height) {
-        this.height = height;
-    }
+@SuppressWarnings({"PMD.UnusedPrivateField"})
+public @Data class TempDerivativeMessage {
+    private final String fileUrl;
+    private final String rotation;
+    private final Integer width;
+    private final Integer height;
 }
