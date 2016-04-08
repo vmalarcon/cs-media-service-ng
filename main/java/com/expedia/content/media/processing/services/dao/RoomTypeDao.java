d016fda75d34b8b6466a4b6fab3d4731e32193d6
mmofidpoor <a-mmofidpoor@expedia.com>
2016-04-08 17:24:44 -0400

    CSPB-532268-expediaId is hard-coded in media-service

--- main/java/com/expedia/content/media/processing/services/dao/RoomTypeDao.java
+++ main/java/com/expedia/content/media/processing/services/dao/RoomTypeDao.java
@@ -34,8 +34,7 @@ public class RoomTypeDao {
         final List<Integer> roomIds = DomainDataUtil.getRoomIds(outerDomain);
         Boolean roomExists = Boolean.TRUE;
         if (outerDomain.getDomain().equals(Domain.LODGING) && !CollectionUtils.isNullOrEmpty(roomIds)) {
-            //todo change back after get expedia id.
-            final Map<String, Object> results = sproc.execute("41098");
+            final Map<String, Object> results = sproc.execute(Integer.parseInt(outerDomain.getDomainId()));
             final List<RoomType> roomTypes = (List<RoomType>) results.get(PropertyRoomTypeGetIDSproc.ROOM_TYPE_RESULT_SET);
 
             final List<Integer> roomTypeCatalogItemIds = roomTypes.stream()
