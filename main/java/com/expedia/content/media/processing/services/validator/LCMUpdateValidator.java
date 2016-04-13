38535c389dcd04022d50eac91b081564cb2562d1
senli <seli@expedia.com>
2016-04-13 12:23:14 -0400

    add validation for subcategory

--- main/java/com/expedia/content/media/processing/services/validator/LCMUpdateValidator.java
+++ main/java/com/expedia/content/media/processing/services/validator/LCMUpdateValidator.java
@@ -1,6 +1,7 @@
 package com.expedia.content.media.processing.services.validator;
 
 import com.expedia.content.media.processing.pipeline.domain.ImageMessage;
+import com.expedia.content.media.processing.services.dao.MediaDomainCategoriesDao;
 import com.expedia.content.media.processing.services.dao.RoomTypeDao;
 import com.expedia.content.media.processing.services.util.ValidatorUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,9 @@ import java.util.Map;
 public class LCMUpdateValidator implements MapMessageValidator {
     @Autowired
     private RoomTypeDao roomTypeDao;
+    @Autowired
+    private MediaDomainCategoriesDao mediaDomainCategoriesDao;
+    private final static String DEFAULT_LANG_ID = "1033";
 
     public List<Map<String, String>> validateImages(List<ImageMessage> messageList) {
         final List<Map<String, String>> list = new ArrayList<>();
@@ -22,6 +26,9 @@ public class LCMUpdateValidator implements MapMessageValidator {
             messageMap.put("imageMessage", imageMessage);
             if (!roomTypeDao.roomTypeCatalogItemIdExists(imageMessage.getOuterDomainData())) {
                 errorMsg.append("The room does not belong to the property in LCM.");
+            }
+            if (!mediaDomainCategoriesDao.subCategoryIdExists(imageMessage.getOuterDomainData(), DEFAULT_LANG_ID)) {
+                errorMsg.append("The category does not exist in LCM.");
             }
             if (errorMsg.length() > 0) {
                 ValidatorUtil.putErrorMapToList(list, errorMsg, imageMessage);
