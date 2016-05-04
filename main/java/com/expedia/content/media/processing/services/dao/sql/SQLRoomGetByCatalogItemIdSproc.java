b8cc04a02ffd73a62279c1a1364cc097159bedbb
Andre Caron <acaron@YULC02P60BZG3QN.sea.corp.expecn.com>
2016-05-04 17:01:15 -0400

    updated sprocs

--- main/java/com/expedia/content/media/processing/services/dao/sql/SQLRoomGetByCatalogItemIdSproc.java
+++ main/java/com/expedia/content/media/processing/services/dao/sql/SQLRoomGetByCatalogItemIdSproc.java
@@ -11,7 +11,7 @@ import org.springframework.jdbc.core.SqlReturnResultSet;
 import org.springframework.stereotype.Repository;
 
 /**
- * Call a MSSQL Sproc [RoomTypeGetByCatalogItemID] in LCM in order to retrieve data from the Media,RoomTypeCatalogItemID, 
+ * Call a MSSQL Sproc [RoomTypeMediaListByPropertyID] in LCM in order to retrieve data from the Media,RoomTypeCatalogItemID, 
  * CatalogItemMedia, and Paragraph (derivatives) tables to get current rooms of all the media id of a property.
  */
 @Repository
@@ -19,7 +19,7 @@ public class SQLRoomGetByCatalogItemIdSproc extends SQLRoomGetSproc {
     
     @Autowired
     public SQLRoomGetByCatalogItemIdSproc(final DataSource dataSource) {
-        super(dataSource, "RoomTypeGetByCatalogItemID");
+        super(dataSource, "RoomTypeMediaListByPropertyID");
         declareParameter(new SqlParameter("@pCatalogItemID", Types.INTEGER));
         declareParameter(new SqlReturnResultSet(ROOM_SET, new RoomRowMapper()));
     }
