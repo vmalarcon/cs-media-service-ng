ee0a1191437a88cf3a7a5083bf8c452c97971d58
martin <a-msigne@expedia.com>
2016-06-01 14:53:44 -0400

    Fixed typo

--- main/java/com/expedia/content/media/processing/services/metrics/MetricProcessor.java
+++ main/java/com/expedia/content/media/processing/services/metrics/MetricProcessor.java
@@ -11,7 +11,6 @@ import java.util.function.BiFunction;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-// import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
