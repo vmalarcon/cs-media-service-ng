f704612e67f28e8c7acf1a5a936a2dbd95cafc41
sstannus <t-sstannus@expedia.com>
2016-06-15 12:29:49 -0400

    Revert "Merge pull request #217 from EWE/feature/CSPB-532575-get-status-from-dynamo" - fix javadocs

--- main/java/com/expedia/content/media/processing/services/MediaController.java
+++ main/java/com/expedia/content/media/processing/services/MediaController.java
@@ -275,7 +275,10 @@ public class MediaController extends CommonServiceController {
      * @param activeFilter         Filter determining what images to return. When true only active are returned. When false only inactive media is returned. When
      *                             all then all are returned. All is set a default.
      * @param derivativeTypeFilter Inclusive filter to use to only return certain types of derivatives. Returns all derivatives if not specified.
+     * @param derivativeCategoryFilter Inclusive filter to use to only return certain types of medias. Returns all medias if not specified.
      * @param headers              Headers of the request.
+     * @param pageSize             Positive integer to filter the number of media displayed per page. pageSize is inclusive with pageIndex.
+     * @param pageIndex            Positive integer to filter the page to display. pageIndex is inclusive with pageSize.
      * @return The list of media data belonging to the domain item.
      * @throws Exception Thrown if processing the message fails.
      */
