d1cfaa9fae984a32a504abbf66cdc57396b8fd2a
mmofidpoor <a-mmofidpoor@expedia.com>
2016-08-01 11:32:05 -0400

    CSPB-533447-catching a generic exception rather than unparsable exception

--- main/java/com/expedia/content/media/processing/services/util/TimeZoneWrapper.java
+++ main/java/com/expedia/content/media/processing/services/util/TimeZoneWrapper.java
@@ -3,7 +3,6 @@ package com.expedia.content.media.processing.services.util;
 import com.expedia.content.media.processing.pipeline.util.TimeZoneUtil;
 import org.apache.commons.lang.StringUtils;
 
-import java.text.ParseException;
 import java.util.Date;
 
 public class TimeZoneWrapper {
@@ -16,7 +15,7 @@ public class TimeZoneWrapper {
         dateAsString = StringUtils.defaultIfEmpty(dateAsString, StringUtils.EMPTY);
         try {
             date = TimeZoneUtil.convertLCMTimeZoneDate(dateAsString);
-        } catch (ParseException ex) {
+        } catch (Exception ex) {
             return date;
         }
         return date;
