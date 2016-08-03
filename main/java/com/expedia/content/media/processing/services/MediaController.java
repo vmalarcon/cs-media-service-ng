9090b2163e7cab08c2779dd6bd944bfe7400e2cb
sstannus <t-sstannus@expedia.com>
2016-08-03 14:52:45 -0400

    CSPB-533048 - refactored code

--- main/java/com/expedia/content/media/processing/services/MediaController.java
+++ main/java/com/expedia/content/media/processing/services/MediaController.java
@@ -195,10 +195,10 @@ public class MediaController extends CommonServiceController {
             final String clientId = auth.getName();
             return processRequest(message, requestID, serviceUrl, clientId, ACCEPTED, timeReceived);
         } catch (ImageMessageException ex) {
-            LOGGER.error(ex, "ERROR ServiceUrl={} RequestId={} ErrorMessage={}", Arrays.asList(serviceUrl, requestID, ex.getMessage()), message);
+            LOGGER.error("ERROR ServiceUrl={} RequestId={} ErrorMessage={}", Arrays.asList(serviceUrl, requestID, ex.getMessage()), ex, message);
             return this.buildErrorResponse("JSON request format is invalid. Json message=" + message, serviceUrl, BAD_REQUEST);
         } catch (Exception ex) {
-            LOGGER.error(ex, "ERROR ServiceUrl={} RequestId={} ErrorMessage={}", Arrays.asList(serviceUrl, requestID, ex.getMessage()), message);
+            LOGGER.error("ERROR ServiceUrl={} RequestId={} ErrorMessage={}", Arrays.asList(serviceUrl, requestID, ex.getMessage()), ex, message);
             poker.poke("Media Services failed to process a mediaAdd request - RequestId: " + requestID, hipChatRoom,
                     message, ex);
             throw ex;
@@ -242,12 +242,12 @@ public class MediaController extends CommonServiceController {
                 return mediaUpdateProcessor.processRequest(imageMessageNew, lcmMediaId, domainId, dynamoMedia);
             }
         } catch (ImageMessageException ex) {
-            LOGGER.error(ex, "ERROR ServiceUrl={} QueryId={} RequestId={} ErrorMessage={}", Arrays.asList(serviceUrl, queryId,
-                    requestID, ex.getMessage()), message);
+            LOGGER.error("ERROR ServiceUrl={} QueryId={} RequestId={} ErrorMessage={}", Arrays.asList(serviceUrl, queryId,
+                    requestID, ex.getMessage()), ex, message);
             return this.buildErrorResponse("JSON request format is invalid. Json message=" + message, serviceUrl, BAD_REQUEST);
         } catch (Exception ex) {
-            LOGGER.error(ex, "ERROR ServiceUrl={} QueryId={} RequestId={} ErrorMessage={}", Arrays.asList(serviceUrl, queryId,
-                    requestID, ex.getMessage()), message);
+            LOGGER.error("ERROR ServiceUrl={} QueryId={} RequestId={} ErrorMessage={}", Arrays.asList(serviceUrl, queryId,
+                    requestID, ex.getMessage()), ex, message);
             poker.poke("Media Services failed to process a mediaUpdate request - RequestId: " + requestID, hipChatRoom,
                     message, ex);
             throw ex;
@@ -647,7 +647,7 @@ public class MediaController extends CommonServiceController {
             LOGGER.info("Sending message to queue done", jsonMessage);
             logActivity(message, Activity.MEDIA_MESSAGE_RECEIVED, null);
         } catch (Exception ex) {
-            LOGGER.error(ex, "Error publishing ErrorMessage={}", Arrays.asList(ex.getMessage()), message);
+            LOGGER.error("Error publishing ErrorMessage={}", Arrays.asList(ex.getMessage()), ex, message);
             throw new RuntimeException("Error publishing message=[" + jsonMessage + "]", ex);
         }
     }
@@ -669,7 +669,7 @@ public class MediaController extends CommonServiceController {
         final List<MapMessageValidator> defaultValidatorList = mapValidatorList.get(DEFAULT_VALIDATION_RULES);
         final List<MapMessageValidator> validatorList = mapValidatorList.getOrDefault(clientId, defaultValidatorList);
         if (validatorList == defaultValidatorList) {
-            LOGGER.warn("NO VALIDATION FOR USER  Action=\"Using default validations\" ClientId={}", Arrays.asList(clientId), imageMessage);
+            LOGGER.warn("NO VALIDATION FOR CLIENT Action=\"Using default validations\" ClientId={}", clientId);
         }
         if (validatorList == null && defaultValidatorList == null) {
             return UNAUTHORIZED_USER_MESSAGE;
