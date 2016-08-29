60261a554972a291cccd4b67d7faf3a81e3369ac
martin <a-msigne@expedia.com>
2016-08-29 16:52:26 -0400

    fix the pmd issues

--- main/java/com/expedia/content/media/processing/services/CustomAuthenticationEntryPoint.java
+++ main/java/com/expedia/content/media/processing/services/CustomAuthenticationEntryPoint.java
@@ -1,19 +1,19 @@
 package com.expedia.content.media.processing.services;
 
-import com.expedia.content.media.processing.pipeline.util.FormattedLogger;
-import expedia.content.solutions.metrics.annotations.Counter;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
+import java.io.IOException;
+import java.security.Principal;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.security.Principal;
+
+import com.expedia.content.media.processing.pipeline.util.FormattedLogger;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import expedia.content.solutions.metrics.annotations.Counter;
 
 /**
  * Customer Authentication class to handle authentication exception.
