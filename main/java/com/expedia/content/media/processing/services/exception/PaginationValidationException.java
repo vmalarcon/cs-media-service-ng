b9e1ce60650291b7d836f6fe1e8d1298fcfd05f7
mmofidpoor <a-mmofidpoor@expedia.com>
2016-09-19 11:55:33 -0400

    CSPB-534000-applying code reviews

--- main/java/com/expedia/content/media/processing/services/exception/PaginationValidationException.java
+++ main/java/com/expedia/content/media/processing/services/exception/PaginationValidationException.java
@@ -1,5 +1,8 @@
 package com.expedia.content.media.processing.services.exception;
 
+/**
+ * This exception is thrown when pagination parameters are wrong
+ */
 
 public class PaginationValidationException extends RuntimeException {
     public PaginationValidationException(String message, Throwable cause) {
